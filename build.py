1bfde1a6c8462ac20f84d0b7f7ee6f5f18d4100f
SSE4 <tomskside@gmail.com>
2018-09-27 14:57:14 +0700

    - shared is not supported on Windows
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -3,9 +3,10 @@
 
 
 from bincrafters import build_template_default
+import platform
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder()
+    builder = build_template_default.get_builder(shared_option_name=('' if platform.system() == 'Windows' else None))
 
     builder.run()
